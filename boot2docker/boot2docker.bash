e86cb769cdf445829a3f3753b522a173338af012
Kristian Glass <git@doismellburning.co.uk>
2015-01-12 12:14:03 +0000

    Don't auto-start boot2docker, just load config if it's running

--- boot2docker/boot2docker.bash
+++ boot2docker/boot2docker.bash
@@ -4,11 +4,8 @@ if [[ -x ${BOOT2DOCKER} ]]
 then
 	ISRUNNING=$(${BOOT2DOCKER} status | grep --silent running ; echo $?)
 
-	if ((0 != ISRUNNING)) 
+	if ((0 == ISRUNNING))
 	then
-		echo "boot2docker not running - attempting to change that"
-		${BOOT2DOCKER} start
+		$(${BOOT2DOCKER} shellinit 2> /dev/null)
 	fi
-
-	$(${BOOT2DOCKER} shellinit 2> /dev/null)
 fi
