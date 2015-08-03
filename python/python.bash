fc7eef0415017b667e679a21852ba811f0a3e339
Kristian Glass <git@doismellburning.co.uk>
2015-08-03 12:55:59 +0100

    The benefits of pyc files to me seem few

--- python/python.bash
+++ python/python.bash
@@ -1,2 +1,6 @@
 export PYTHONSTARTUP="$HOME/.pythonrc"
 export PYTHONWARNINGS="default" #http://docs.python.org/dev/whatsnew/2.7.html#the-future-for-python-2-x - DeprecationWarnings are desirable!
+
+# https://docs.python.org/2/using/cmdline.html#envvar-PYTHONDONTWRITEBYTECODE
+# Thanks MarkusH
+export PYTHONDONTWRITEBYTECODE=1
