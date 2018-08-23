73cd755902bb24b82a9188f1ccdf8a21fd83f704
Kristian Glass <git@doismellburning.co.uk>
2018-08-23 13:33:34 +0100

    Drop PYTHONWARNINGS for general usage
    I use so many third-party Python CLI tools now that this generates
    distressing amounts of spam :(

--- python/python.bash
+++ python/python.bash
@@ -1,5 +1,4 @@
 export PYTHONSTARTUP="$HOME/.pythonrc"
-export PYTHONWARNINGS="default" #http://docs.python.org/dev/whatsnew/2.7.html#the-future-for-python-2-x - DeprecationWarnings are desirable!
 
 # https://docs.python.org/2/using/cmdline.html#envvar-PYTHONDONTWRITEBYTECODE
 # Thanks MarkusH
